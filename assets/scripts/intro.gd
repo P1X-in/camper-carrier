31b890b8fe6d8f71166f30ce1b3cfd7e3d58298a
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2019-01-26 19:24:11 +0100

    intro

---
+++ assets/scripts/intro.gd
@@ -0,0 +1,13 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene("assets/scenes/game.tscn")
