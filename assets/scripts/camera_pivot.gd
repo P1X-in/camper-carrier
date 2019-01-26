bbc2ef8ee56d6694f08608465b01c2b1188b1e71
CzlowiekImadlo <quak@tlen.pl>
2019-01-26 13:39:45 +0100

    Increased camera speed

--- assets/scripts/camera_pivot.gd
+++ assets/scripts/camera_pivot.gd
@@ -1,6 +1,6 @@
 extends Position3D
 
-export var rotate_speed = 1.0
+export var rotate_speed = 1.2
 export var return_speed = 1.0
 
 const DEADZONE = 0.25
@@ -12,7 +12,6 @@ var _angle_y = 0
 var axis_value
 var axis_abs
 
-var cumulative = 0
 
 func _process(delta):
     if angle_y != _angle_y:
@@ -32,17 +31,6 @@ func _physics_process(delta):
             # ROTATE LEFT - RIGHT
             if axis == JOY_ANALOG_RX:
                 angle_y -= rotate_speed * axis_value
-                cumulative = 0
-
-            #if axis == JOY_ANALOG_LY:
-            #    var rotation_axis_value = Input.get_joy_axis(0, JOY_ANALOG_RX)
-            #    if abs(rotation_axis_value) < DEADZONE and cumulative > 0.5:
-            #        if axis_value > 0:
-            #            angle_y = 180
-            #        else:
-            #            angle_y = 0
-            #    else:
-            #        cumulative += delta
 
 
 
