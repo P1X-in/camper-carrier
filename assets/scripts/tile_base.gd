afef48eb2cdcf660afeb335f67853215343d2230
CzlowiekImadlo <quak@tlen.pl>
2019-01-26 20:44:01 +0100

    Extended base tile scripting

--- assets/scripts/tile_base.gd
+++ assets/scripts/tile_base.gd
@@ -1,13 +1,10 @@
 extends MeshInstance
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func get_level(num):
+    return self.get_node("level" + str(num))
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_basic():
+    return self.get_level(1)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_upgrade(current_level):
+    return self.get_level(current_level + 1)
---
+++ assets/scripts/tile_level.gd
@@ -0,0 +1,6 @@
+extends Spatial
+
+export var name = ""
+export var description = ""
+export var cost_sausage = 0
+export var cost_beer = 0
